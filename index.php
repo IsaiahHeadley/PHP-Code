56cfd0a45a9f23b6f812b777350d6858c8ab1e5a
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2018-12-05 17:22:46 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	echo "this is PHP";
+
+
+
+?>
+<!DOCTYPE >
+<html xml:lang="en" lang="en">
+    <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>Title here</title>
+			<link rel="stylesheet" href="styles.css">
+    </head>
+
+<body> 
+    <h1>Web page content goes here.</h1>
+	<p>This is a paragraph.</p>
+</body>
+</html>
