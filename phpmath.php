34f18e684e0f5e96cf980685da0194cc1c373eb8
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2021-01-13 12:43:50 -0500

    Add files via upload

---
+++ phpmath.php
@@ -0,0 +1,26 @@
+<?php
+$num1 = 8;
+$num2 = 6;
+echo 'Number 1 is ';
+echo $num1 ,PHP_EOL;
+//echo "<br/>";
+echo 'Number 2 is ';
+echo $num2,PHP_EOL;
+//echo "<br/>";
+//Addition
+echo 'Numb1 + Numb2 is..';
+echo $num1 + $num2,PHP_EOL;
+//echo "<br/>";
+//Subtraction
+echo 'Numb1 - Numb2 is..';
+echo $num1 - $num2,PHP_EOL;
+//echo "<br/>";
+//Multiplication
+echo 'Numb1 * Numb2 is..';
+echo $num1 * $num2,PHP_EOL;
+//echo "<br/>";
+//Division
+echo 'Numb1 / Numb2 is..';
+echo $num1 / $num2,PHP_EOL;
+//echo "<br/>";
+?>
