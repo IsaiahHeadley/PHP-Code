3c464255b0854a3bb3693dd955543fa63ead1a26
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2021-11-10 12:34:32 -0500

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+define("TITLE", "PHP Variables & Constants");
+?>
+<DOCTYPE html>
+<html>
+<head>
+<title> <?php echo TITLE; ?> </title>
+</head>
+<body>
+    <?php
+    //BOOLEAN: Variable True or False
+    $loggedIn = true;
+    //Integer: Whole Number
+    $myAge    = 30;
+    //Floating Point: Number with decimal 
+    $totalPrice = 150.70;
+    //String: Letters and number expression
+    $fullName = "Isaiah Charles";
+    //POST is an array
+    $_POST 
+    echo "Hello my name is $fullName and I am $myAge years old!"
+    
+        
+    ?>
+</body>
+</html>
